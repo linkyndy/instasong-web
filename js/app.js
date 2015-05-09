ec1d7393baa27de89a871dd8007e4b19c0a7c7c3
linkyndy <linkyndy@gmail.com>
2015-05-09 15:48:00 +0300

    Small changes to see that gh-pages get updated

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return ['red', 'yellow', 'blue', 'green'];
   }
 });
 
